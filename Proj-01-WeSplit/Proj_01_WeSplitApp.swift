dcd59c315a458b8c1322bce55a86f91e40e9b352
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-06 16:42:58 +0300

    Initial Commit

---
+++ Proj-01-WeSplit/Proj_01_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proj_01_WeSplitApp.swift
+//  Proj-01-WeSplit
+//
+//  Created by Андрей Бородкин on 06.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Proj_01_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
